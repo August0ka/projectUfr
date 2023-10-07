73167600d92685f3d8da2d14476c49e8ae568ab5
August0ka <kiichi_sassagima@outlook.com>
2023-10-07 16:19:16 -0400

    add/database

--- novoUsuario.php
+++ novoUsuario.php
@@ -1,27 +1,53 @@
-<form action= "?page=salvar" method="POST">
-    <input type="hidden" name="acao" value="cadastrar">
-    <div class="mb-3">
-        <label>Nome </label>
-        <input type="text"  name="nome" class="form-control">
+<form>
+  <!-- Email input -->
+  <div class="form-outline mb-4">
+    <input type="email" id="form2Example1" class="form-control" />
+    <label class="form-label" for="form2Example1">Email address</label>
+  </div>
+
+  <!-- Password input -->
+  <div class="form-outline mb-4">
+    <input type="password" id="form2Example2" class="form-control" />
+    <label class="form-label" for="form2Example2">Password</label>
+  </div>
+
+  <!-- 2 column grid layout for inline styling -->
+  <div class="row mb-4">
+    <div class="col d-flex justify-content-center">
+      <!-- Checkbox -->
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+        <label class="form-check-label" for="form2Example31"> Remember me </label>
+      </div>
     </div>
 
-     <div class="mb-3">
-        <label>E-mail </label>
-         <input type="email"  name="email" class="form-control">
+    <div class="col">
+      <!-- Simple link -->
+      <a href="#!">Forgot password?</a>
     </div>
-     <div class="mb-3">
-        <label>Senha </label>
-         <input type="password"  name="senha" class="form-control">
-    </div>
-     <div class="mb-3">
-        <label>Data de Nascimento</label>
-         <input type="date"  name="data_nasc" class="form-control">
-    </div>
-    <div class="mb-3">
-        <button type="submit" class="btn btn-primary">
-        Salvar
-        </button>
+  </div>
 
-    </div>
+  <!-- Submit button -->
+  <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+  <!-- Register buttons -->
+  <div class="text-center">
+    <p>Not a member? <a href="#!">Register</a></p>
+    <p>or sign up with:</p>
+    <button type="button" class="btn btn-link btn-floating mx-1">
+      <i class="fab fa-facebook-f"></i>
+    </button>
+
+    <button type="button" class="btn btn-link btn-floating mx-1">
+      <i class="fab fa-google"></i>
+    </button>
+
+    <button type="button" class="btn btn-link btn-floating mx-1">
+      <i class="fab fa-twitter"></i>
+    </button>
+
+    <button type="button" class="btn btn-link btn-floating mx-1">
+      <i class="fab fa-github"></i>
+    </button>
+  </div>
 </form>
-
