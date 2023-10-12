dd3b0c03ad16fd5c11d61305fefb673c1144e622
miriarezende <goncalvesmiria19@gmail.com>
2023-10-12 13:50:29 -0400

    updateDataBase

--- clinic.sql
+++ clinic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Out-2023 às 18:56
+-- Tempo de geração: 12-Out-2023 às 19:48
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -62,8 +62,9 @@ CREATE TABLE `consult` (
 --
 
 INSERT INTO `consult` (`id`, `doctors_id`, `patient_id`, `description`, `data`, `hour`, `active`) VALUES
-(1, 1, 1, 'teste', '2023-10-09', '12:00:00.000000', 0),
-(2, 2, 1, 'teste', '2023-10-09', '11:00:00.000000', 1);
+(2, 1, 1, 'Avaliação física do paciente', '2023-10-09', '13:00:00.000000', 1),
+(3, 4, 1, 'Paciente com neoplasia óssea.', '2024-03-25', '13:30:00.000000', 0),
+(9, 2, 1, 'Limpeza dentária', '2023-10-26', '14:30:00.000000', 1);
 
 -- --------------------------------------------------------
 
@@ -86,9 +87,11 @@ CREATE TABLE `doctors` (
 --
 
 INSERT INTO `doctors` (`id`, `CRM`, `name`, `email`, `phone`, `expertise`, `active`) VALUES
-(1, 90254, 'Marcelo Antunes', 'marcantun@marci.com', '35996811523', 'Pediatra', 1),
-(2, 10532, 'Meire Arnaldo', 'meirevane@mail.com', '6698643254', 'Odontologista', 1),
-(3, 54236, 'BIZERRO', 'bizerrinho@mail.com', '9963255100', 'ginecologista', 0);
+(1, 90254, 'Marcelo Antunes', 'marcantun@marci.com', '(35) 99681-1523', 'Pediatra', 1),
+(2, 10532, 'Meire Arnaldo', 'meirevane@mail.com', '(66) 98643-254', 'Odontologista', 1),
+(3, 54236, 'BIZERRO', 'bizerrinho@mail.com', '9963255100', 'ginecologista', 0),
+(4, 5329055, 'Antonio Luiz Andrade', 'antonidrade@mail.com', '(33) 62054-5188', 'Ortopedista', 1),
+(5, 386034, 'Luiz Silva', 'campos@mail.com', '(77) 56600-5463', 'Odontologista', 0);
 
 -- --------------------------------------------------------
 
@@ -111,8 +114,10 @@ CREATE TABLE `patient` (
 --
 
 INSERT INTO `patient` (`id`, `CPF`, `name`, `email`, `phone`, `address`, `active`) VALUES
-(1, '01236589412', 'Rogerio Marq', 'marqgerio@outk.com', '4596123258', 'R Farias, n° 25, Jd Matriz, Nova Iorque - Mt', 1),
-(2, '25423615420', 'Carlos Farias', 'cadfaz@mail.com', '56999613245', 'R Franciline, n° 50, Jd Antonieta, Nova Mutum - MT', 0);
+(1, '012.365.894-12', 'Rogerio Marques', 'marqgerio@outk.com', '(45) 96123-258', 'R Farias, n° 25, Jd Matriz, Nova Iorque', 1),
+(2, '25423615420', 'Carlos Farias', 'cadfaz@mail.com', '56999613245', 'R Franciline, n° 50, Jd Antonieta, Nova Mutum - MT', 0),
+(3, '451.294.226-10', 'Marta Lopes', 'martloop@mail.com', '(88) 65221-0054', 'R Martins, Rondonópolis', 1),
+(4, '567.680.564', 'Marco Gabriel Martins', 'marcgab@mail.com', '(66) 73828-5041', '', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -158,19 +163,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT de tabela `consult`
 --
 ALTER TABLE `consult`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para despejos de tabelas
@@ -181,7 +186,7 @@ ALTER TABLE `patient`
 --
 ALTER TABLE `consult`
   ADD CONSTRAINT `fk_doctors` FOREIGN KEY (`doctors_id`) REFERENCES `doctors` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_patients` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_patients` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
